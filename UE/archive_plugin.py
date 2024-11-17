6325e2b6537e9e661b45577d90deef5066a812e1
Gradess <trofimovjava@gmail.com>
2024-11-17 14:41:38 +0100

    Add better build-plugin pipeline

--- UE/archive_plugin.py
+++ UE/archive_plugin.py
@@ -4,22 +4,19 @@ from utils import Utils
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--package-directory", "-pd", type=str, required=True)
-arg_parser.add_argument("--append-version", "-s", action="store_true", required=False)
 args = arg_parser.parse_args()
 
 package_directory = args.package_directory
-append_version = args.append_version
 
 print("Package directory: ", package_directory)
-print("Append version: ", append_version)
 
 plugin_descriptor = Utils.get_plugin_descriptor_path(package_directory)
 print("Plugin descriptor: ", plugin_descriptor)
 
-engine_version = Utils.extract_engine_version(package_directory) if append_version else ""
+engine_version = Utils.extract_engine_version(package_directory)
 print("Engine version: ", engine_version)
 
-archive_path = rf'{package_directory}{("_" + engine_version) if append_version else ""}.zip'
+archive_path = rf'{package_directory}{("_" + engine_version)}.zip'
 print("Archive path: ", archive_path)
 
 if os.path.exists(archive_path):
--- UE/build_plugin.py
+++ UE/build_plugin.py
@@ -9,7 +9,6 @@ arg_parser.add_argument("--unreal-directory", "-ud", type=str, required=True)
 arg_parser.add_argument("--plugin-directory", "-pd", type=str, required=True)
 arg_parser.add_argument("--destination", "-d", type=str, required=True)
 arg_parser.add_argument("--submission", "-s", action="store_true", required=False)
-arg_parser.add_argument("--save-package-name", "-spn", action="store_true", required=False)
 args = arg_parser.parse_args()
 
 
@@ -17,18 +16,15 @@ unreal_directory = args.unreal_directory
 plugin_directory = args.plugin_directory
 destination = args.destination
 submission = args.submission
-save_package_name = args.save_package_name
 
 print("Unreal directory: ", unreal_directory)
 print("Plugin directory: ", plugin_directory)
 print("Destination: ", destination)
 print("Submission: ", submission)
-print("Save package name: ", save_package_name)
 
 
 unreal_version = re.search(r"(\d+\.\d+)", unreal_directory).group(1)
 print("Unreal version: ", unreal_version)
-
 
 plugin_descriptor = Utils.get_plugin_descriptor_path(plugin_directory)
 print("Plugin descriptor: ", plugin_descriptor)
@@ -36,19 +32,8 @@ print("Plugin descriptor: ", plugin_descriptor)
 plugin_name = re.search(r"(\w+).uplugin", plugin_descriptor).group(1)
 print("Plugin name: ", plugin_name)
 
-
-package_name = ""
-if submission:
-    package_name = rf"{destination}\{plugin_name}Submission"
-else:
-    package_name = rf"{destination}\{plugin_name}"
-
+package_name = rf"{destination}\{plugin_name}"
 print("Package name: ", package_name)
-
-if save_package_name:
-    print("Saving package name...")
-    with open(rf"{plugin_directory}\package_name.txt", "w") as f:
-        f.write(package_name)
 
 run_uat_bat = rf"{args.unreal_directory}\Engine\Build\BatchFiles\RunUAT.bat"
 build_plugin_cmd = rf'{run_uat_bat} BuildPlugin -Rocket -Plugin="{plugin_descriptor}" -Package="{package_name}"'
@@ -58,27 +43,21 @@ print("Build plugin command: ", build_plugin_cmd)
 return_code = os.system(build_plugin_cmd)
 
 if return_code == 0 and submission:
+    submission_package_name = rf"{destination}\{plugin_name}Submission"
+    print("Submission package name: ", submission_package_name)
+
+    print("Copying package to submission package...")
+    shutil.copytree(package_name, submission_package_name)
+
     print("Deleting Binaries and Intermediate folders...")
-    binaries_path = os.path.join(package_name, "Binaries")
-    intermediate_path = os.path.join(package_name, "Intermediate")
+    binaries_path = os.path.join(submission_package_name, "Binaries")
+    intermediate_path = os.path.join(submission_package_name, "Intermediate")
 
     if os.path.exists(binaries_path):
         shutil.rmtree(binaries_path)
 
     if os.path.exists(intermediate_path):
         shutil.rmtree(intermediate_path)
-
-config_path_packaged = os.path.join(package_name, "Config")
-
-if not os.path.exists(config_path_packaged):
-    print("Warning: Config folder not found in packaged plugin!")
-    config_path = os.path.join(plugin_directory, "Config")
-    print("Config path: ", config_path)
-    print("Trying to copy Config folder from plugin directory...")
-    if os.path.exists(config_path):
-        shutil.copytree(config_path, os.path.join(package_name, "Config"))
-    else:
-        print("Warning: Config folder not found in plugin directory!")
 
 if return_code != 0:
     print("Error!")
